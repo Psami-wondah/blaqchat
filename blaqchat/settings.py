78a3cfdfba782f00a656e61b90b6c7751a326128
Psami-wondah <okechukwusamuel16@gmail.com>
2022-08-18 17:36:20 +0100

    added pgbouncer shii

--- blaqchat/settings.py
+++ blaqchat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +84,12 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=0, ssl_require=False)
+DATABASES["default"].update(db_from_env)
+DATABASES["default"].update({"DISABLE_SERVER_SIDE_CURSORS": True})
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -124,3 +131,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+django_heroku.settings(locals(), databases=False)
